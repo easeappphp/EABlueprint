1c07adf629c2e0791f55658143d30eb1e8a9087d
raghuveer <raghuveer.dendukuri@gmail.com>
2021-05-03 21:42:41 +0530

    updates
    - update to controller & model

--- app/Http/Controllers/AllUserDetails/GetController.php
+++ app/Http/Controllers/AllUserDetails/GetController.php
@@ -5,6 +5,8 @@ namespace EaseAppPHP\EABlueprint\App\Http\Controllers\AllUserDetails;
 
 use \EaseAppPHP\EABlueprint\App\Models\AllUserDetails\Get;
 
+use \EaseAppPHP\Foundation\BaseWebView;
+
 class GetController extends \EaseAppPHP\EABlueprint\App\Http\Controllers\WebController
 {
     
@@ -16,8 +18,20 @@ class GetController extends \EaseAppPHP\EABlueprint\App\Http\Controllers\WebCont
     public function index()
     {
         //echo "Welcome to EaseApp OOP";
-		$getModel = new Get();
-		echo $getModel->index();
+		$getModel = new Get($this->matchedRouteDetails, $this->queryParams);
+		echo $getModel->index() . "\n";
+		//print_r($parametersArray);
+		print_r($this->matchedRouteDetails);
+		echo "\n";
+		print_r($this->queryParams);
+		echo "\n";
+		
+		$filename = "myfile.php";
+		$dataArray = array("name" => "srirama", "place" => "ayodhya");
+		
+		$getView = new BaseWebView($filename, $dataArray);
+		
+		$getView->render();
 		
     }
     
--- app/Models/AllUserDetails/Get.php
+++ app/Models/AllUserDetails/Get.php
@@ -7,7 +7,18 @@ namespace EaseAppPHP\EABlueprint\App\Models\AllUserDetails;
 class Get extends \EaseAppPHP\Foundation\BaseWebModel
 {
     
-    /**
+    protected $matchedRouteDetails;
+	protected $queryParams;
+
+	public function __construct($matchedRouteDetails, $queryParams)
+	{
+		
+		$this->matchedRouteDetails = $matchedRouteDetails;
+		$this->queryParams = $queryParams;
+		
+	}
+	
+	/**
      * Process Ajax/API Call
      *
      * @return array
@@ -15,6 +26,11 @@ class Get extends \EaseAppPHP\Foundation\BaseWebModel
     public function index()
     {
         echo "Welcome to EaseApp OOP - MODEL";
+		echo "\nINSIDE MODEL:\n";
+		print_r($this->matchedRouteDetails);
+		echo "\n";
+		print_r($this->queryParams);
+		echo "\n";
     }
     
     
