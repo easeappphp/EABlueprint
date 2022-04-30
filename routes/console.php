39ef7f3c41f6ddea07c7d1733f413fdbae1d6ba5
Raghu Veer Dendukuri <raghuveer.dendukuri@gmail.com>
2022-04-30 12:57:19 +0530

    cleanup

--- routes/console.php
+++ routes/console.php
@@ -81,37 +81,4 @@ return [
 		'controller_class_name' => \EaseAppPHP\EABlueprint\App\Console\Controllers\ProceduralCliController::class,
 		'method_name' => 'cliTextOutput'
 	],
-	/*'admin-panel-adss-ta-udzrr-dns-records-list' => [
-		'route_value' => '/admin-panel/app-deployment/app-id/:routing_eng_var_4/switchover-step/:routing_eng_var_6/dns-records/list',
-		'port' => '',
-		'description' => '',
-		'status' => 'ON',
-		'number_of_records' => '10000',
-		'number_of_loops_count' => '2',
-		'sleep_time_minimum_seconds' => '2',
-		'sleep_time_maximum_seconds' => '4',
-		'sleep_interval_definition' => 'min',//min | max | random
-		'page_filename' => 'admin-panel-adss-ta-udzrr-dns-records-list.php',
-		'route_type' => 'backend-web-app', //cron-job | message-queue-worker | command
-		'controller_type' => 'procedural',
-		'controller_class_name' => \EaseAppPHP\EABlueprint\App\Console\Controllers\ProceduralCliController::class,
-		'method_name' => 'cliTextOutput'
-	],
-	'admin-panel-adss-ta-udzrr-dns-record-add' => [
-		'route_value' => '/admin-panel/app-deployment/app-id/:routing_eng_var_4/switchover-step/:routing_eng_var_6/dns-record/add',
-		'port' => '',
-		'description' => '',
-		'status' => 'ON',
-		'number_of_records' => '10000',
-		'number_of_loops_count' => '2',
-		'sleep_time_minimum_seconds' => '2',
-		'sleep_time_maximum_seconds' => '4',
-		'sleep_interval_definition' => 'min',//min | max | random
-		'page_filename' => 'admin-panel-adss-ta-udzrr-dns-record-add.php',
-		'route_type' => 'backend-web-app', //cron-job | message-queue-worker | command
-		'controller_type' => 'procedural',
-		'controller_class_name' => \EaseAppPHP\EABlueprint\App\Console\Controllers\ProceduralCliController::class,
-		'method_name' => 'cliTextOutput'
-	],*/
-	
 ];
