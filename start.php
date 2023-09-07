ba9ea9ab63768e58521ebc27148727617b627490
raghuveer <raghuveer.dendukuri@gmail.com>
2023-09-07 10:30:46 +0530

    some changes

---
+++ start.php
@@ -0,0 +1,68 @@
+<?php
+declare(strict_types=1);
+
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+/*
+*--------------------------------------------------------------------------
+* Prevent Direct Access
+*--------------------------------------------------------------------------
+*
+* This is to prevent direct access to a file inside public root or public_html or www folder.
+*
+*/
+defined('START') or define("START", "No Direct Access");
+
+/*
+*--------------------------------------------------------------------------
+* Build Foundation - Register the Auto Loader
+*--------------------------------------------------------------------------
+*
+* Composer provides a convenient, automatically generated class loader to autoload files for
+* this application.
+*
+*/
+require_once __DIR__ . '/vendor/autoload.php';
+
+use EaseAppPHP\App;
+
+/*
+*--------------------------------------------------------------------------
+* Construct your Dream Building - Bootstrap the Application
+*--------------------------------------------------------------------------
+*
+* Bootstrap the building, i.e., load dependencies and connect them to the PSR-11 compatible dependency injection container.
+*
+*/
+global $app; // workerman
+
+$container = require_once __DIR__.'/bootstrap/app.php';
+
+$application = new App($container);
+$container->instance('App', $application);
+
+$app = $container->get('App');
+
+$app->init();
+
+/*
+*--------------------------------------------------------------------------
+* Enjoy the moments - Run the Application
+*--------------------------------------------------------------------------
+*
+* Once we have the application, we can handle incoming server requests for web (web pages / Ajax calls / REST APIs / SOAP
+* APIs) with a response back to the client's browser.
+*
+*/
+
+function run(): string
+{
+    global $app;
+    ob_start();
+
+    $app->run();
+
+    return ob_get_clean();
+}
